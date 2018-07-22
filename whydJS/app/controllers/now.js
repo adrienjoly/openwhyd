35bee142cdc4fb437206001161a90860e526c370
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-22 14:00:15 +0200

    delete back-end and non-needed files

--- whydJS/app/controllers/now.js
+++
@@ -1,3 +0,0 @@
-exports.controller = function(request, reqParams, response) {
-	response.render(Date.now());
-}
--- whydJS/app/templates/uiSnippets.js
+++
@@ -1,9 +0,0 @@
-// uiSnippets => moved to snip.js
-
-var snip = require('../snip.js');
-
-exports.replaceURLWithHTMLLinks = snip.replaceURLWithHTMLLinks;
-exports.replaceURLWithFullHTMLLinks = snip.replaceURLWithFullHTMLLinks;
-exports.shortenURLs = snip.shortenURLs;
-exports.htmlEntities = snip.htmlEntities;
-exports.renderTimestamp = snip.renderTimestamp;
